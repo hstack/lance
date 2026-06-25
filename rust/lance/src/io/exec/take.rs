57ceabd691466e0bb4816a0c59c0ddc2523580a3
Andrei Dragomir <adragomi@adobe.com>
2026-06-25 15:01:04 +0300

    HSTACK: add output_projection getter for TakeExec for recreation

--- rust/lance/src/io/exec/take.rs
+++ rust/lance/src/io/exec/take.rs
@@ -553,6 +553,8 @@ impl TakeExec {
     pub fn dataset(&self) -> &Arc<Dataset> {
         &self.dataset
     }
+
+    pub fn output_projection(&self) -> &Projection { &self.output_projection }
 }
 
 impl ExecutionPlan for TakeExec {
